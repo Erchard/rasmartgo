2d90820bf4e419eb4638486db9f143bbcb5ec081
root <rnOOBaxbC1>
2019-02-02 01:51:21 +0200

    Main

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"github.com/Erchard/rasmartgo/counters"
-	"github.com/Erchard/rasmartgo/rsapi"
 	"log"
 )
 
 func main() {
 	log.Println("Hello Rasmart!")
 	counters.GetCounters()
-	rsapi.Main()
 	log.Println("End")
 
 }
