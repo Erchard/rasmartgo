3eb3d95f30b844dba7c5a16b6a0385ea1727b5bf
root <rnOOBaxbC1>
2019-02-01 20:56:32 +0200

    test pubkey

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"encoding/hex"
+	"log"
+)
 
 func main() {
-	log.Printf("Hello Rasmart!")
+	log.Println("Hello Rasmart!")
+
+	const_pubkey, err := hex.DecodeString("50f86b12dbdb50ae9197980787198e278dc9ec94ec8491e3b79df03157ad0bd1")
+	if err != nil {
+		log.Fatal("Parse pubkey error")
+	}
+	log.Println("Pub key len: ", len(const_pubkey))
 }
