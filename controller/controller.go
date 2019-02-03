3b346996be1fe717abf776b3a4a60cdc6036050f
root <rnOOBaxbC1>
2019-02-03 07:11:06 +0200

    HTTP server

--- controller/controller.go
+++ controller/controller.go
@@ -1,1 +1,37 @@
 package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Erchard/rasmartgo/counters"
+	"log"
+	"net/http"
+)
+
+func ServerStart() {
+
+	http.HandleFunc("/counters", countersHandler)
+	http.HandleFunc("/", mainHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func mainHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>RASMART</title>
+</head>
+<body>
+<h1>Hello Rasmart!</h1>
+</body>
+</html>
+`)
+}
+
+func countersHandler(writer http.ResponseWriter, request *http.Request) {
+	raw := counters.GetCounters()
+	json, _ := json.Marshal(raw)
+	fmt.Fprintf(writer, "%s", json)
+}
---
+++ controller/controller_test.go
@@ -0,0 +1,7 @@
+package controller
+
+import "testing"
+
+func TestServerStart(t *testing.T) {
+	ServerStart()
+}
